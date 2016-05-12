07832ff5472f46535d1d4f68c739e1171f758563
John Nunemaker <nunemaker@gmail.com>
2016-05-12 15:45:33 -0400

    Hide UI forms if gate deactivated

--- spec/flipper/ui/actions/feature_spec.rb
+++ spec/flipper/ui/actions/feature_spec.rb
@@ -56,4 +56,79 @@ RSpec.describe Flipper::UI::Actions::Feature do
       expect(last_response.body).to include("Percentage of Actors")
     end
   end
+
+  describe "GET /features/:feature with boolean gate deactivated" do
+    before do
+      flipper[:search].deactivate :boolean
+      get "/features/search"
+    end
+
+    it "responds with success" do
+      expect(last_response.status).to be(200)
+    end
+
+    it "renders template" do
+      expect(last_response.body).to include("The boolean gate is disabled for this feature.")
+    end
+  end
+
+  describe "GET /features/:feature with percentage_of_actors gate deactivated" do
+    before do
+      flipper[:search].deactivate :percentage_of_actors
+      get "/features/search"
+    end
+
+    it "responds with success" do
+      expect(last_response.status).to be(200)
+    end
+
+    it "renders template" do
+      expect(last_response.body).to include("The percentage of actors gate is disabled for this feature.")
+    end
+  end
+
+  describe "GET /features/:feature with percentage_of_time gate deactivated" do
+    before do
+      flipper[:search].deactivate :percentage_of_time
+      get "/features/search"
+    end
+
+    it "responds with success" do
+      expect(last_response.status).to be(200)
+    end
+
+    it "renders template" do
+      expect(last_response.body).to include("The percentage of time gate is disabled for this feature.")
+    end
+  end
+
+  describe "GET /features/:feature with group gate deactivated" do
+    before do
+      flipper[:search].deactivate :group
+      get "/features/search"
+    end
+
+    it "responds with success" do
+      expect(last_response.status).to be(200)
+    end
+
+    it "renders template" do
+      expect(last_response.body).to include("The groups gate is disabled for this feature.")
+    end
+  end
+
+  describe "GET /features/:feature with actor gate deactivated" do
+    before do
+      flipper[:search].deactivate :actor
+      get "/features/search"
+    end
+
+    it "responds with success" do
+      expect(last_response.status).to be(200)
+    end
+
+    it "renders template" do
+      expect(last_response.body).to include("The individual actors gate is disabled for this feature.")
+    end
+  end
 end
