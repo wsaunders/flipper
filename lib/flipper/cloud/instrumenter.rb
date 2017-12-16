bc810a04331eef9db971f24adbd9d5e375d89d34
John Nunemaker <nunemaker@gmail.com>
2017-12-16 15:20:58 -0500

    Use cloud instrumenter processor and event for cloud instrumenter

--- lib/flipper/cloud/instrumenter.rb
+++ lib/flipper/cloud/instrumenter.rb
@@ -1,4 +1,6 @@
 require 'thread'
+require 'flipper/cloud/instrumenter/event'
+require 'flipper/cloud/instrumenter/processor'
 
 module Flipper
   module Cloud
@@ -7,10 +9,17 @@ module Flipper
 
       def initialize(configuration)
         @configuration = configuration
+        @processor = Processor.new(configuration)
       end
 
       def instrument(name, payload = {}, &block)
-        @configuration.instrumenter.instrument(name, payload, &block)
+        result = instrumenter.instrument(name, payload, &block)
+        @processor.add Event.new(name, payload)
+        result
+      end
+
+      def instrumenter
+        @configuration.instrumenter
       end
     end
   end
