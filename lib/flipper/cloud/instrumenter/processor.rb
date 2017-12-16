db2c7fa7c0bb1becc681882556c3250deb18b157
John Nunemaker <nunemaker@gmail.com>
2017-12-16 15:20:40 -0500

    Add super boring cloud processor

---
+++ lib/flipper/cloud/instrumenter/processor.rb
@@ -0,0 +1,15 @@
+module Flipper
+  module Cloud
+    class Instrumenter
+      class Processor
+        def initialize(configuration)
+          @configuration = configuration
+        end
+
+        def add(event)
+
+        end
+      end
+    end
+  end
+end
---
+++ spec/flipper/cloud/instrumenter/processor_spec.rb
@@ -0,0 +1,6 @@
+require 'helper'
+require 'flipper/cloud/instrumenter/processor'
+
+RSpec.describe Flipper::Cloud::Instrumenter::Processor do
+
+end
