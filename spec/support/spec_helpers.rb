d3c1cc881a3e4be538bbf9eff2141a342f62a105
John Nunemaker <nunemaker@gmail.com>
2017-12-23 13:40:21 -0500

    Add default event_receiver in specs

--- spec/support/spec_helpers.rb
+++ spec/support/spec_helpers.rb
@@ -1,10 +1,12 @@
 require 'json'
+require 'flipper/event_receivers/noop'
 require 'flipper/adapters/memory'
 require 'rack/test'
 
 module SpecHelpers
   def self.included(base)
     base.let(:flipper) { build_flipper }
+    base.let(:event_receiver) { build_event_receiver }
     base.let(:app) { build_app(flipper) }
   end
 
@@ -24,6 +26,10 @@ module SpecHelpers
 
   def build_memory_adapter
     Flipper::Adapters::Memory.new
+  end
+
+  def build_event_receiver
+    Flipper::EventReceivers::Noop
   end
 
   def json_response
