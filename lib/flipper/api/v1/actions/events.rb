2b02d6d4190de795f87ab371732c7afa4fb29da6
John Nunemaker <nunemaker@gmail.com>
2017-12-23 14:45:14 -0500

    Minor formatting

--- lib/flipper/api/v1/actions/events.rb
+++ lib/flipper/api/v1/actions/events.rb
@@ -13,20 +13,24 @@ module Flipper
           def post
             # TODO: validate the entire request before we try to do anything
             # with it
-            batch = Batch.new(ip: request.ip,
-                              pid: json_params.fetch('pid'),
-                              hostname: json_params.fetch('hostname'),
-                              user_agent: request.user_agent,
-                              version: json_params.fetch('version'),
-                              platform: json_params.fetch('platform'),
-                              platform_version: json_params.fetch('platform_version'),
-                              event_capacity: json_params.fetch('event_capacity'),
-                              event_flush_interval: json_params.fetch('event_flush_interval'),
-                              client_timestamp: json_params.fetch('client_timestamp'),
-                              timestamp: Cloud::Instrumenter.clock_milliseconds,
-                              events: json_params.fetch('events').map do |hash|
-                                        Cloud::Instrumenter::Event.from_hash(hash)
-                                      end)
+            events = json_params.fetch('events').map do |hash|
+              Cloud::Instrumenter::Event.from_hash(hash)
+            end
+            attributes = {
+              ip: request.ip,
+              pid: json_params.fetch('pid'),
+              hostname: json_params.fetch('hostname'),
+              user_agent: request.user_agent,
+              version: json_params.fetch('version'),
+              platform: json_params.fetch('platform'),
+              platform_version: json_params.fetch('platform_version'),
+              event_capacity: json_params.fetch('event_capacity'),
+              event_flush_interval: json_params.fetch('event_flush_interval'),
+              client_timestamp: json_params.fetch('client_timestamp'),
+              timestamp: Cloud::Instrumenter.clock_milliseconds,
+              events: events,
+            }
+            batch = Batch.new(attributes)
             event_receiver.call(batch)
             json_response({}, 201)
           end
