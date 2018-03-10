a6d08498ddc83bc8e692834f86868be27249b514
John Nunemaker <nunemaker@gmail.com>
2018-03-10 13:48:55 -0500

    Simplify cloud long running example

--- examples/cloud/long_running.rb
+++ examples/cloud/long_running.rb
@@ -1,9 +1,7 @@
 require File.expand_path('../../example_setup', __FILE__)
 
 require 'flipper/cloud'
-require 'flipper/adapters/active_support_cache_store'
-require 'active_support/cache'
-require 'active_support/cache/memory_store'
+require 'active_support/notifications'
 
 token = ENV.fetch("TOKEN") { abort "TOKEN environment variable not set." }
 feature_name = ENV.fetch("FEATURE") { "testing" }.to_sym
@@ -13,13 +11,7 @@ Flipper.configure do |config|
     Flipper::Cloud.new(token) do |cloud|
       cloud.url = "http://localhost:5000/adapter"
       cloud.debug_output = STDOUT
-      cloud.adapter do |adapter|
-        Flipper::Adapters::ActiveSupportCacheStore.new(adapter,
-          ActiveSupport::Cache::MemoryStore.new, {expires_in: 60.seconds})
-      end
-      cloud.event_capacity = 10_000
-      cloud.event_flush_interval = 5
-      cloud.event_batch_size = 1_000
+      cloud.instrumenter = ActiveSupport::Notifications
     end
   end
 end
@@ -27,11 +19,6 @@ end
 actor = Flipper::Actor.new("User;23")
 
 loop do
-  begin
-    Flipper.enabled?(feature_name, actor)
-  rescue
-
-  end
-
+  Flipper.enabled?(feature_name, actor)
   sleep 0.1
 end
